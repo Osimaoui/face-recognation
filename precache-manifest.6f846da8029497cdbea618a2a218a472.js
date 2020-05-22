155875a1732835de32c9a120423deadeb60f0932
Osimaoui <otmane.assaka@gmail.com>
2020-05-22 01:43:40 +0000

    Updates

---
+++ precache-manifest.6f846da8029497cdbea618a2a218a472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba876ff41691d9ceaa8cc25b044ef551",
+    "url": "/face-recognation/index.html"
+  },
+  {
+    "revision": "c3859b9c1fa5be05648a",
+    "url": "/face-recognation/static/css/2.bfce28e8.chunk.css"
+  },
+  {
+    "revision": "0b7aeac380b719034822",
+    "url": "/face-recognation/static/css/main.9e4072b9.chunk.css"
+  },
+  {
+    "revision": "c3859b9c1fa5be05648a",
+    "url": "/face-recognation/static/js/2.036bed69.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognation/static/js/2.036bed69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b7aeac380b719034822",
+    "url": "/face-recognation/static/js/main.2fc78074.chunk.js"
+  },
+  {
+    "revision": "6f4903ee2caf542c9075",
+    "url": "/face-recognation/static/js/runtime-main.76d8f7de.js"
+  }
+]);
